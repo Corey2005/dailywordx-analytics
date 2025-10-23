2b15b789b362f43678ad8e8003649fdeab88ef09
Corey2005 <corey@mjscs.com.au>
2025-10-23 22:33:14 +1100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { HashRouter } from 'react-router-dom'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
- <React.StrictMode>
-    <BrowserRouter basename="/dailywordx-analytics/">
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
