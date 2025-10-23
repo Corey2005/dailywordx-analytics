7b4301e007332dae6c9b8c65d212f865bb0aaab7
Corey2005 <corey@mjscs.com.au>
2025-10-23 21:43:30 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  // very important for Vite + TSX
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
