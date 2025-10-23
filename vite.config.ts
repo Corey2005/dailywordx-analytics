04b286eb816b03252b13c44009d0a7188b8bbd36
Corey2005 <corey@mjscs.com.au>
2025-10-23 23:31:38 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dailywordx-analytics/', // <-- repo name
+  base: '/dailywordx-analytics/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {},
+    // prevent Vite from adding crossorigin
+    assetsDir: 'assets',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-  },
+  server: {
+    cors: false, // ensure dev server doesn’t mess with headers
+  }
 })
